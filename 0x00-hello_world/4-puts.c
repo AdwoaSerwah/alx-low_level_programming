3f735492e5e12ed65e77ec4777bc40a6ebc81a8b
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-08-11 03:52:03 +0000

    Execute task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the message
+ * "Programming is like building a multilingual puzzle
+ * using the puts function and ends with a return value of 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
