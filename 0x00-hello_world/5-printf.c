a98febc3f7e72760de7a45e6626afc05dd75f80f
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-08-11 04:53:16 +0000

    Use the betty style to comment in task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,16 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: This program prints the message
- * 'with proper grammar, but the outcome is a piece of art,',
- * followed by a new line using the printf function.
+ * Description: This program uses the printf function
+ * to display the message
+ * 'with proper grammar, but the outcome is a piece of art,'
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
