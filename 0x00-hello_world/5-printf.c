192fd80708c77b8e96bd0a55bac57db636c1a585
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-08-11 04:03:02 +0000

    Execute task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the message
+ * 'with proper grammar, but the outcome is a piece of art,',
+ * followed by a new line using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
