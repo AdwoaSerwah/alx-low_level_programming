a1efc4b0b9d21195b92bd8e20e8178a46b9cde91
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-08-12 03:24:46 +0000

    Execute task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all possible
+ * different combination of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1 = 0;
+
+	while (num1 <= 8)
+	{
+		int num2 = num1 + 1;
+
+		while (num2 <= 9)
+		{
+			putchar('0' + num1);
+			putchar('0' + num2);
+			if (num1 < 8 || num2 < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
