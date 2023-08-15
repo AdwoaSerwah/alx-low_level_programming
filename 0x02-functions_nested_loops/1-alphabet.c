4c199a33d645ca9abc49893da08fecebf8bd31e6
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-08-15 18:58:49 +0000

    Submit task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: This program prints the alphabets in lowercase,
+ * followed by a newline
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+}
