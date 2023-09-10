ad16e7f451457750bce4a942003c389f8855ec2b
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-09-10 03:37:02 +0000

    Add task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to char
+ * @s: Double pointer
+ * @to: Final value
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
