65d9f96cbefee1693b86b622261da1dc66ae5f28
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-09-11 15:22:03 +0000

    Add task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,11 +7,14 @@
  * @age: Age
  * @owner: Owner
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
