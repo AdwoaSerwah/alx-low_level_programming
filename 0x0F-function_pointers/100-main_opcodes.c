43bde88ef4a2e284be0d8d3687256275dcdbc2c8
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-09-13 18:59:24 +0000

    Add task 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+    int num_bytes;
+    int i;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    num_bytes = atoi(argv[1]);
+
+    if (num_bytes < 0)
+    {
+        printf("Error\n");
+        return (2);
+    }
+
+    unsigned char *main_ptr = (unsigned char *)main;
+
+    for (i = 0; i < num_bytes; i++)
+    {
+        printf("%02x", main_ptr[i]);
+        if (i < num_bytes - 1)
+            printf(" ");
+        else
+            printf("\n");
+    }
+
+    return (0);
+}
+
