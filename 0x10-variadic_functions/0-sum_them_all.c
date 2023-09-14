a4b76a0111c33a0435b8f38c1696aa3ed901d70d
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-09-14 23:17:51 +0000

    Add task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,9 +3,9 @@
 
 /**
  * sum_them_all - Returns the sum of all its parameters
- * n: First operand
+ * @n: Count of all arguments
  *
- * Returns: sum of all the parameters or 0 if n == 0
+ * Return: sum of all the parameters or 0 if n == 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
