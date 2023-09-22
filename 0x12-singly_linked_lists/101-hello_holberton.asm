f190941dde9db8f674480529c5f68d2e7e5098d1
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-09-22 17:56:07 +0000

    Add task 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,15 @@
 section .data
-	format_hello db "Hello, Holberton",10,0
+	hello_format db "Hello, Holberton",10,0
+	hello_args dq 0
 
 section .text
 	global main
-
 	extern printf
 
 main:
-	push rdi
-	push rsi
-	lea rdi, [rel format_hello]
-	xor rax, rax
+	mov rdi, hello_format
+	mov rax, 0
 	call printf
-	pop rsi
-	pop rdi
-
 	mov rax, 60
 	xor rdi, rdi
 	syscall
