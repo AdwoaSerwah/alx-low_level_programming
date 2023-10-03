6adb09fb24ce293bd2433af58ab124ce88cf3ada
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-10-03 03:22:03 +0000

    Add task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Filename
+ * @text_content: Content
+ *
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int desc_sk, output;
+
+	if (filename == NULL)
+		return (-1);
+	desc_sk = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (desc_sk == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		size_t len_sk = 0;
+
+		while (*text_content != '\0')
+		{
+			len_sk++;
+			text_content++;
+		}
+		output = write(desc_sk, text_content, len_sk);
+
+		if (output == -1)
+		{
+			close(desc_sk);
+			return (-1);
+		}
+	}
+	close(desc_sk);
+	return (1);
+}
