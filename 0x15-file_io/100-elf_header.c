357deb0cdeddd28879ec71c1919a45e404383584
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-10-03 05:15:21 +0000

    Soon

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Display information contained in the ELF header of an ELF file.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ * Return: 0 on success, 98 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
