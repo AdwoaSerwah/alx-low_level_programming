f5766fd1dee8fa61fc5c0361835f4397124bf3fc
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-10-03 02:42:35 +0000

    Add task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int desc_skk;
+	ssize_t bt_read, bt_wrttn;
+	char *letters_store;
+
+	if (filename == NULL)
+		return (0);
+	desc_skk = open(filename, O_RDONLY);
+	if (desc_skk == -1)
+		return (0);
+	letters_store = malloc(sizeof(char) * letters);
+	if (letters_store == NULL)
+	{
+		close(desc_skk);
+		return (0);
+	}
+	bt_read = read(desc_skk, letters_store, letters);
+	if (bt_read == -1)
+	{
+		close(desc_skk);
+		free(letters_store);
+		return (0);
+	}
+	bt_wrttn = write(STDOUT_FILENO, letters_store, bt_read);
+	if (bt_wrttn == -1 || bt_wrttn != bt_read)
+	{
+		close(desc_skk);
+		free(letters_store);
+		return (0);
+	}
+	close(desc_skk);
+	free(letters_store);
+	return (bt_wrttn);
+}
