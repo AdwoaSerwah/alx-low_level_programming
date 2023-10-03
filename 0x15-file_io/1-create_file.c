4bf2bff2c6e7cc16b0a320e5c4e83cb5df1a82fd
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-10-03 03:32:35 +0000

    Add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,10 +21,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		size_t len_sk = 0;
 
-		while (*text_content != '\0')
+		while (text_content[len_sk] != '\0')
 		{
 			len_sk++;
-			text_content++;
 		}
 		output = write(desc_sk, text_content, len_sk);
 
