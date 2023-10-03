a6fa14af4a4fbdd70c1e47af05ba53c63fa9a98b
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-10-03 04:21:45 +0000

    Add task 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,11 @@
 #include <fcntl.h>
 
 #define NULL ((void *)0)
+#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
+void cp(int orig, int dest_sk);
 
 #endif /* MAIN_H */
