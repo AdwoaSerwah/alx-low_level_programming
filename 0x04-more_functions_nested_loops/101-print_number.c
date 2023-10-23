50000e1745ecb0136bbd2340f5a377884c65d035
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-10-23 02:38:21 +0000

    Add task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
  * print_number - Prints an integer using _putchar
@@ -6,19 +8,12 @@
  */
 void print_number(int n)
 {
-	int exponent = 1;
-	int temp;
+	int exponent = 1, temp, quotient;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
-	}
-
-	if (n > 9 || n < 0)
-	{
-		temp = n / 10;
-
+		temp = -(n / 10);
 		while (temp != 0)
 		{
 			exponent = exponent * 10;
@@ -27,8 +22,24 @@ void print_number(int n)
 		temp = n;
 		while (exponent != 0)
 		{
-			int quotient = temp / exponent;
-
+			quotient = -(temp / exponent);
+			_putchar(quotient + '0');
+			temp = temp % exponent;
+			exponent = exponent / 10;
+		}
+	}
+	else if (n > 9)
+	{
+		temp = n / 10;
+		while (temp != 0)
+		{
+			exponent = exponent * 10;
+			temp /= 10;
+		}
+		temp = n;
+		while (exponent != 0)
+		{
+			quotient = temp / exponent;
 			_putchar(quotient + '0');
 			temp = temp % exponent;
 			exponent = exponent / 10;
