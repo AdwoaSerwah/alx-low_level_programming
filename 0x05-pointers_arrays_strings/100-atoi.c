0cfe5fb46fedf305cb7cd0674f80eb4a56c2c508
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-10-24 01:59:49 +0000

    Add task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -30,10 +30,10 @@ int _atoi(char *s)
 		if ((result > 0 && result > (INT_MAX - digit) / 10) ||
 			(result < 0 && result < (INT_MIN + digit) / 10))
 		{
-			return (sign == 1) ? (INT_MAX) : (INT_MIN);
+			return ((sign == 1) ? (INT_MAX) : (INT_MIN));
 		}
 		result = result * 10 + (sign * digit);
 		s++;
 	}
-	return result;
+	return (result);
 }
