88a1647d29e91d97b67e68a6bed0a54ec81d8dc4
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-10-28 01:21:00 +0000

    Wait

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,24 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 20
-
-int main() {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    const int charset_length = sizeof(charset) - 1;
-    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
-
-    srand(time(NULL));
-
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        int index = rand() % charset_length;
-        password[i] = charset[index];
-    }
-
-    password[PASSWORD_LENGTH] = '\0';
-
-    printf("Generated Password: %s\n", password);
-
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	return (0);
 }
 
