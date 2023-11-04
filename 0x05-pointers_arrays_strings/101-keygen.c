e8b10528780739bc8ed6adf6450eb6a24c6ade67
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-04 01:49:01 +0000

    Add task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,11 +4,32 @@
 
 /**
  * main - Entry point
+ * Description: Generates random valid passwords for 101-crackme
  *
  * Return: 0
  */
 int main(void)
 {
+	char str[23];
+	int i = 0;
+	int len = sizeof(str) - 1;
+
+	srand(time(NULL));
+	while (i < len)
+	{
+		char ch = (char)(rand() % 128);
+
+		str[i] = ch;
+		i++;
+	}
+	str[i] = '\0';
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = '~';
+		i++;
+	}
+	printf("%s", str);
 	return (0);
 }
-
