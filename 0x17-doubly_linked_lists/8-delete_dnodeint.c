464ebeb3fc569674badeb3b27b5b6a218e1b64fe
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-10 03:20:27 +0000

    Try 8 again

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,37 +5,37 @@
  * @head: Pointer
  * @index: Index
  *
- * Return: 1 on success, -1 on error.
+ * Return: 1 on success, -1
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int counter = 0;
-	dlistint_t *present, *temp_sk;
+	unsigned int i = 0;
+	dlistint_t *pres_sk = *head;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (-1);
-
-	present = *head;
-
 	if (index == 0)
 	{
-		*head = present->next;
-		free(present);
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(pres_sk);
 		return (1);
 	}
-
-	while (present != NULL && counter < index - 1)
+	while (i < index)
 	{
-		present = present->next;
-		counter++;
+		if (pres_sk == NULL)
+			return (-1);
+
+		pres_sk = pres_sk->next;
+		i++;
 	}
-
-	if (present == NULL || present->next == NULL)
+	if (pres_sk == NULL)
 		return (-1);
-
-	temp_sk = present->next;
-	present->next = temp_sk->next;
-	free(temp_sk);
-
+	if (pres_sk->prev != NULL)
+		pres_sk->prev->next = pres_sk->next;
+	if (pres_sk->next != NULL)
+		pres_sk->next->prev = pres_sk->prev;
+	free(pres_sk);
 	return (1);
 }
