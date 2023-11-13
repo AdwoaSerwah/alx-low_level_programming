08c34fd5d88ce882ea6b12c4c3f23d092f14be45
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-13 23:40:24 +0000

    Add 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-gcc -Wall -fPIC -c *.c
+files=$(find . -name "*.c" | grep -v "main.c" | tr '\n' ' ')
+gcc -Wall -fPIC -c $files
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
